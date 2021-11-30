e0a5ad5131e37222642d7515538b818b57bfde37
Hrybeniuk Kateryna <dp040497see@gmail.com>
2021-11-30 18:56:08 +0200

    edit js file

---
+++ index.js
@@ -0,0 +1,6 @@
+const firstNum = prompt('Enter the first number'); 
+const secondNum = prompt('Enter the second number');
+const resultNum = Number(firstNum) + Number(secondNum);
+console.log (resultNum);
+alert (`${firstNum} + ${secondNum} = ${resultNum}`);
+
